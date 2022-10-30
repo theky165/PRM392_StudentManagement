344ec63ad80db569326132067abbfc8be6d4ea13
VietAnh1108 <caisswind@gmail.com>
2022-10-30 18:12:13 +0700

    (update) managementStudent

--- app/src/main/java/com/example/prm392_studentmanagement/helper/DateTimeHelper.java
+++ app/src/main/java/com/example/prm392_studentmanagement/helper/DateTimeHelper.java
@@ -1,4 +1,19 @@
 package com.example.prm392_studentmanagement.helper;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateTimeHelper {
+     private static final  String Pattern = "dd/MM/yyy";
+     public static Date toDate(String st) throws ParseException {
+         SimpleDateFormat sdf = new SimpleDateFormat(Pattern);
+
+         return  sdf.parse(st);
+     }
+     public static String toString(Date date){
+         SimpleDateFormat sdf = new SimpleDateFormat(Pattern);
+
+         return  sdf.format(date);
+     }
 }
