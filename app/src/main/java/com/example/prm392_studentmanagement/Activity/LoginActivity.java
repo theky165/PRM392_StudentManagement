8fc2a46b3945949c052588783b92b031aa103ac0
VietAnh1108 <caisswind@gmail.com>
2022-10-30 11:39:20 +0700

    (new) login

---
+++ app/src/main/java/com/example/prm392_studentmanagement/Activity/LoginActivity.java
@@ -0,0 +1,47 @@
+package com.example.prm392_studentmanagement.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.prm392_studentmanagement.MainActivity;
+import com.example.prm392_studentmanagement.R;
+import com.google.android.material.snackbar.Snackbar;
+
+public class LoginActivity extends AppCompatActivity  implements View.OnClickListener {
+    EditText etUser,etPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.prm392_studentmanagement.R.layout.activity_login);
+        findViewById(R.id.btnLogin).setOnClickListener(this);
+        findViewById(R.id.btnExit).setOnClickListener(this);
+        etUser = findViewById(R.id.etUser);
+        etPassword = findViewById(R.id.etPass);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnLogin:
+                if (etUser.getText().toString().equals("admin")
+                        && etPassword.getText().toString().equals("123"))
+                {
+                    Intent intent =  new Intent(this, MainActivity.class);
+                    startActivity(intent);
+                }else
+                {
+                    Snackbar.make(view,"Wrong UserName or Password!",Snackbar.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.btnExit:
+                finish();
+                break;
+        }
+
+    }
+}
