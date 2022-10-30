9deaced1fb8bd954c75cce80dab5e5e09bbe0146
theky165 <theky165@gmail.com>
2022-10-30 16:33:08 +0700

    [update] main activity

--- app/src/main/java/com/example/prm392_studentmanagement/MainActivity.java
+++ app/src/main/java/com/example/prm392_studentmanagement/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -11,9 +12,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btnNewClass).setOnClickListener(this);
+        findViewById(R.id.btnClasses).setOnClickListener(this);
+        findViewById(R.id.btnListStudents).setOnClickListener(this);
+        findViewById(R.id.btnLogout).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnNewClass:
+                Toast.makeText(this, "New class", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnClasses:
+                break;
+            case R.id.btnListStudents:
+                break;
+            case R.id.btnLogout:
+                break;
+        }
     }
 }
