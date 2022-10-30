430cc4cf34ec28d398b9aab2eb65f9878933d6f0
Dangthaihunga3 <Dangthaihunga3@gmail.com>
2022-10-30 12:34:46 +0700

    [update] main view

--- app/src/main/java/com/example/prm392_studentmanagement/MainActivity.java
+++ app/src/main/java/com/example/prm392_studentmanagement/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.prm392_studentmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
     }
 }
